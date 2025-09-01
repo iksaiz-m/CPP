36b14e155ff870d2bae353b324c48bfe343a031c
Iker Saiz Martinez <iksaiz-m@c4r3s1.42urduliz.com>
2025-09-01 21:31:20 +0200

    started ex04

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,22 @@
+#include "sed.hpp"
+
+
+void sedforlosers(std::ofstream& file, std::string s1, std::string s2)
+{
+    file.
+}
+
+int main(std::string filename, std::string s1, std::string s2)
+{
+    std::ofstream file;
+    // std::string amigo;
+    // open(filename);
+    file.open(filename);
+    if (file.is_open())
+        sedforlosers(file, s1, s2);
+    // fopen(filename);
+    // fread(filename)
+    // .erase(s1)
+    std::cin
+     
+}
---
+++ cpp01/ex04/sed.hpp
@@ -0,0 +1,11 @@
+#ifndef SED_HPP
+#define SED_HPP
+
+#include <iostream>
+#include <cctype>
+#include <string.h>
+#include <sstream>
+#include <iomanip>
+#include <fstream>
+
+#endif
