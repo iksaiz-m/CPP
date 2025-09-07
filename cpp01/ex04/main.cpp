0188924df0ac342bdb5e2287a85e3c2960a7599b
iker <saizmartineziker@gmail.com>
2025-09-07 12:03:50 +0200

    finished reading, replacing and writing in files

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,22 +1,45 @@
 #include "sed.hpp"
 
-
-void sedforlosers(std::ofstream& file, std::string s1, std::string s2)
+std::string replace_line(std::string line, std::string s1, std::string s2)
 {
-    file.
+    size_t pos = 0;
+    while ((pos = line.find(s1, pos)) != std::string::npos)
+    {
+        line.erase(pos, s1.length());
+        line.insert(pos, s2);
+        pos = pos + s2.length();
+    }
+    return (line);
 }
 
-int main(std::string filename, std::string s1, std::string s2)
+int main(int ac, char **av)
 {
-    std::ofstream file;
-    // std::string amigo;
-    // open(filename);
-    file.open(filename);
-    if (file.is_open())
-        sedforlosers(file, s1, s2);
-    // fopen(filename);
-    // fread(filename)
-    // .erase(s1)
-    std::cin
-     
+    if (ac != 4)
+        return (std::cout << "Error: use 3 arguments" << std::endl, 1);
+    std::string filename = (av[1]);
+    std::string s1 = av[2];
+    std::string s2 = av[3];
+    if (filename.empty())
+        return (std::cout << "Error, there should be a filename" << std::endl, 1);
+    std::ifstream file_copy(filename.c_str());
+    if (file_copy.is_open())
+    {
+        std::ofstream file_replace(filename.append(".replace").c_str());
+        if (!file_replace.is_open())
+            return (std::cout << "Error: couldn't create output file" << std::endl, 1);
+        if (s1.empty() || s2.empty())
+            file_replace << file_copy.rdbuf();
+        else
+        {
+            std::string line;
+            while (std::getline(file_copy, line))
+            {
+                line = replace_line(line, s1, s2);
+                file_replace << line << '\n';
+            }
+        }
+    }
+    else
+        return (std::cout << "Error, couldnt open " << filename << std::endl, 1);
+     return (0);
 }
--- cpp01/ex04/sed.hpp
+++ cpp01/ex04/sed.hpp
@@ -2,10 +2,10 @@
 #define SED_HPP
 
 #include <iostream>
-#include <cctype>
-#include <string.h>
-#include <sstream>
-#include <iomanip>
+// // #include <cctype>
+// #include <string.h>
+// #include <sstream>
+// #include <iomanip>
 #include <fstream>
 
 #endif
