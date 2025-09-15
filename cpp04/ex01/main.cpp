120e9819a3c4c6c2fe6b5855856705db88e1e4e7
Iker Saiz Martinez <iksaiz-m@c4r4s1.42urduliz.com>
2025-09-15 19:41:47 +0200

    Finished creating and deleting the array of objects without the compiler crying

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -4,35 +4,24 @@
 
 int main()
 {
-// const Animal* meta = new Animal();
-// const Animal* j = new Dog();
-// const Animal* i = new Cat();
-// // const Animal* x(i); // will not call copy constructor as it a pointer not an object, only one delete
-
-// delete (i);
-// i = j; // to make a copy we need to delete de malloc first to avoid leaks after
-
-// std::cout << j->getType() << " " << std::endl;
-// std::cout << i->getType() << " " << std::endl;
-// i->makeSound(); //will output the cat sound!
-// j->makeSound();
-// // x->makeSound();
-// // x->getType();
-// meta->makeSound();
-// delete (j);
-// delete(meta);
-// delete(x); // it can be x or i because it is a pointer
     int n = 10;
     int i = 0;
-    const Animal *animalito = new Animal[n];
+    const Animal *animalito[n];
     // const Animal *animalito = new Cat[n];
 
     while (i < n)
     {
-        animalito[i].getType();
-        // else
-        //     Animal *pepe = new Cat();
+        if (i % 2)
+            animalito[i] = new Dog();
+        else
+            animalito[i] = new Cat();
         i++;
     }
-    delete [] animalito;
+    i = 0;
+    while(i < n)
+    {
+        delete animalito[i];
+        i++;
+    }
+    // delete [] animalito; // cant be done compiler cries a lot
 }
