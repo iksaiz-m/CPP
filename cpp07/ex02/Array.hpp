cfc38b1c06e17024b63aca416a027b24ee8ae621
iker <saizmartineziker@gmail.com>
2025-11-13 22:05:57 +0100

    finished class template

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -12,51 +12,102 @@ class Array
 {
 private:
     T *elements;
+    size_t _size; // size of the array must be actualized every time it changes
 public:
     Array<T>(); // creates an empty array
     Array<T>(unsigned int n); // creates an array of n elements
-    // Array<T>(Array<T>const &other);
-    // Array &operator=(Array<T>const &other);
+    Array<T>(const Array<T> &other);
+    Array<T>&operator=(const Array<T> &other);
     ~Array<T>();
-    // size_t <T>size();
+    size_t size() const;
+    void printelements() const;
+    T& operator[](size_t index);
+    const T& operator[](size_t index) const;
 };
 
 //type <> is needed when using class templates that way compiler know which type of array its creating
 template <typename T>
-Array<T>::Array(void) : elements(NULL)
+Array<T>::Array(void) : elements(NULL), _size(0)
 {
     std::cout << "An empty array has been created" << std::endl;
 }
 
 template <typename T>
-Array<T>::Array(unsigned int n)
+Array<T>::Array(unsigned int n) : elements(NULL), _size(n)
 {
-    size_t i = 0;
-    while(i < n)
-    {
-        elements[i] = new[T] T();
-        std::cout << elements[i] << std::endl;
-        i++;
-    }
+
+    if (n > 0)
+        this->elements = new T[n];
+    std::cout << "An array of size " << n << " has been created" << std::endl;
 }
-// Array<class type>::Array(const Array &other)
-// {
 
-// }
+template <typename T>
+Array<T>::Array(const Array<T> &other) : elements(NULL), _size(0)
+{
+    size_t len = other.size();
+    if (len > 0)
+    {
+        this->elements = new T[len]; 
+        this->_size = len;
+        size_t i = 0;
+        while (i < len)
+        {
+            this->elements[i] = other.elements[i];
+            i++;
+        }
+    }
+    std::cout << "An array of size " << len << " has been created using the copy operator" << std::endl;
+}
 
-// Array<class type>&::operator=(const Array &other)
-// {
+template <typename T>
 
-// }
+Array<T>&Array<T>::operator=(const Array<T> &other)
+{
+    if (this != &other)
+    {
+        // if (this->elements) // Checks if there i   s memory allocated already
+        // turns out it doesnt matter if the pointer is null
+        delete[] this->elements; // To allocate new memory first we delete the pointer with delete[] 
+        this->elements = new T[other.size()];
+        this->_size = other.size();
+        size_t i = 0;
+        while (i < _size)
+        {
+            this->elements[i] = other.elements[i];
+            i++;
+        }
+        std::cout << "An array of size " << _size << " has been created using the assignment operator" << std::endl;
+    }
+    return (*this);
+}
+
 template <typename T>
 Array<T>::~Array()
 {
+    delete[] elements;
+    std::cout << "The Array has been destroyed" << std::endl;
 }
 
-// template <typename T>
-// size_t Array<T>::size(void)
-// {
-    
-// }
+template <typename T>
+size_t Array<T>::size(void) const
+{
+    return (this->_size);
+}
+
+template <typename T>
+T& Array<T>::operator[](size_t index)
+{
+    if (index >= _size)
+        throw std::out_of_range("Index out of bounds");
+    return (this->elements[index]);
+}
+
+template <typename T>
+const T& Array<T>::operator[](size_t index) const
+{
+    if (index >= _size)
+        throw std::out_of_range("Index out of bounds");
+    return (this->elements[index]);
+}
 
 #endif
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -3,6 +3,27 @@
 int main()
 {
     // the type is needed in between <>
-    Array<int> intarray; 
-    Array<char> chararray(5);
+    try
+    {
+        Array<int> intarray;
+        Array<int> meme(5);
+        // meme[0] = 45;
+
+        Array<int> xd(meme);
+        // Array<char> pepe(xd); cant be made because Type and Type arent the same
+        intarray = meme;
+        intarray[1] = 17;
+        // size_t len = meme.size();
+        size_t len2 = xd.size();
+        // std::cout << "tamaño meme: " <<len << std::endl;
+        std::cout << "tamaño xd: " << len2 << std::endl;
+        // std::cout << "xd[0]: " << xd[0] << std::endl;
+        // std::cout << "intarray[0]: " << intarray[0] << std::endl;
+        // std::cout << "intarray[1]: " << intarray[1] << std::endl;
+
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 }
